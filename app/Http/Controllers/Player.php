28ed8e532b10acfbb3fab350576a32fc2e3f1b0a
Mazen ALkhadra <mazen.alkhadra@outlook.com>
2023-05-18 15:02:25 +0400

    Fix

--- app/Http/Controllers/Player.php
+++ app/Http/Controllers/Player.php
@@ -95,7 +95,8 @@ class Player extends Controller
 
   public function getDetails(PlayerModel $player) {
     if(!empty($player->user_id))
-        $player->user = UserModel::query()->select()->whereKey($player->user_id)->get();
+        $player->user = UserModel::query()->select()->whereKey($player->user_id)
+          ->get()->first();
 
     return $player;
   }
