cae746ebc9dbfe46b0f4a79b2c76c2c616a6141d
Mazen ALkhadra <mazen.alkhadra@outlook.com>
2023-09-19 14:44:27 +0400

    User Activities

--- routes/api.php
+++ routes/api.php
@@ -105,4 +105,12 @@ Route::prefix('home')->middleware('auth:sanctum')->group(function() {
 Route::prefix('user')->middleware('auth:sanctum')->group(function() {
   Route::put('/info', [User::class, 'updateInfo']);
   Route::delete('/account', [User::class, 'delete']);
+
+  Route::get('/activities', function (Request $req) {
+    $events = (new Event())->index($req);
+    $matches = (new MatchController())->index($req, ">=,0");
+    $teams = (new Team())->index($req, true);
+
+    return ["matches" => $matches, "events" => $events, "teams"=> $teams];
+  });
 });
